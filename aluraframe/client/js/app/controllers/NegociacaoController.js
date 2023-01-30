547160999bcb30b597aaadc7c45c5403d8c59e7a
otavio-araujo-jardim <otavioajardim@gmail.com>
2023-01-30 07:34:52 -0300

    Corrige valor padrao da negociação

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -33,7 +33,7 @@ class NegociacaoController {
     _limpaFormulario() {
         this._inputData.value = "";
         this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0;
+        this._inputValor.value = "0.0";
         
         this._inputData.focus();
     }
